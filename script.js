e8a075435ed116ff1620aa7a269cfa9fe00d59b7
Charlie <112635823+charliepain@users.noreply.github.com>
2026-01-15 11:38:33 -0500

    Implement fetch and process weather data

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("init");
+async function getWeatherData(location) {
+    try {
+        const response = await fetch(
+            `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=XKG3RWDBXC4S88GBXU4QWHFBY&contentType=json`
+        );
+        const weatherData = await response.json();
+        return weatherData;
+    } catch (err) {
+        console.log(`Error getWeatherData("${location}")`);
+        throw err;
+    }
+}
+
+function processWeatherData(weatherData) {
+    const currentWeather = weatherData.currentConditions;
+    const {datetime, temp, humidity, precip, snow, windspeed, conditions} = currentWeather;
+    return {datetime, temp, humidity, precip, snow, windspeed, conditions};
+}
+
+const processedWeatherData = getWeatherData("London")
+    .then((weatherData) => console.log(processWeatherData(weatherData)));
